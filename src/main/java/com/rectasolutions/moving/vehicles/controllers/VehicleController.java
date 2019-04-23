6ae611997665e3128ffb9762a697c4291e0d242e
Zulfuqar <zulfuqar.aliyev1991@gmail.com>
2019-04-23 22:29:30 +0400

    removed unused import

--- src/main/java/com/rectasolutions/moving/vehicles/controllers/VehicleController.java
+++ src/main/java/com/rectasolutions/moving/vehicles/controllers/VehicleController.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
